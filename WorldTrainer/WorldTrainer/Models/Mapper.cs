1b1f666c7f3ae920f2cfd63bb461057e0f007747
EMraan619 <emraanshaikh619@gmail.com>
2021-10-01 12:21:02 +0530

    Added trainer model and mappre

---
+++ WorldTrainer/WorldTrainer/Models/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldTrainer.Models;
+using Data.Entities;
+
+namespace WorldTrainer.Models
+{
+    public class Mapper
+    {
+        public static TrainerModel Map(Trainer trainer)
+        {
+            return new TrainerModel()
+            {
+                Username = trainer.Username,
+                Password = trainer.Password
+            };
+        }
+    }
+}
---
+++ WorldTrainer/WorldTrainer/Models/TrainerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Entities;
+
+namespace WorldTrainer.Models
+{
+    public class TrainerModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
